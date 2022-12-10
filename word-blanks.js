9c3c191b1d0f45a35ab6c6c62f9677e23379d89e
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2022-12-10 19:19:56 +0300

    Update and rename word-blanks.txt to word-blanks.js

--- word-blanks.js
+++ word-blanks.js
@@ -2,7 +2,4 @@ const myNoun = "dog";
 const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
-
-// Only change code below this line
 const wordBlanks = "My" + " " + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb; // Change this line
-// Only change code above this line
