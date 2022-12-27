bc64f22ca9b30ce5dcea5716ec7a8dd561fd6185
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2022-12-27 17:09:53 +0300

    Create while.js

---
+++ while.js
@@ -0,0 +1,18 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+console.log(myArray)
+
+const ourArray = [];
+let i = 0;
+
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
